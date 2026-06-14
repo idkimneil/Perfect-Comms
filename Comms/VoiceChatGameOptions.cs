4023587390ca24f9f6ff13820702b3b53174c789
artriy <artriy@users.noreply.github.com>
2026-06-14 08:31:34 +0300

    Use exact TOU Mira role colors and match neighbor label format for Vampire and Lovers team radio

--- Comms/VoiceChatGameOptions.cs
+++ Comms/VoiceChatGameOptions.cs
@@ -60,11 +60,11 @@ public class VoiceChatGameOptions
         {
             Visible = TeamRadioSubOptionsVisible
         };
-        TeamRadioVampires = new ToggleHolder(cfg, Section, "TeamRadioVampires", "Team Radio - <color=#E0344A>Vampires</color>", true)
+        TeamRadioVampires = new ToggleHolder(cfg, Section, "TeamRadioVampires", "<color=#A32929><b>Vampire</b></color>: Team Radio", true)
         {
             Visible = TeamRadioSubOptionsVisible
         };
-        TeamRadioLovers = new ToggleHolder(cfg, Section, "TeamRadioLovers", "Team Radio - <color=#FF66CC>Lovers</color>", true)
+        TeamRadioLovers = new ToggleHolder(cfg, Section, "TeamRadioLovers", "<color=#FF66CC><b>Lovers</b></color>: Team Radio", true)
         {
             Visible = TeamRadioSubOptionsVisible
         };
